793bf9ae9e72728a55f51108a6c034b8663a240e
Priyadharshini K <priyadharshinik1901@gmail.com>
2025-11-21 21:24:33 +0530

    Create Testbench.v

---
+++ Moore-Finite-State-Machine/Moore-Finite-State-Machine-Non-overlapping/Testbench.v
@@ -0,0 +1,22 @@
+module MOORE_TESTBENCH;
+    reg clk = 0, rst, in;
+    wire out;
+  
+    MOORE_OVER_1011 moore(.in(in), .clk(clk), .rst(rst), .out(out));
+
+    always #5 clk = ~clk;
+
+    initial begin
+        $monitor($time," in=%b, clk=%b, rst=%b, out=%b", in, clk, rst, out);
+        rst = 1; in = 0;
+        #10 rst = 0;
+        in = 1; #10;
+        in = 0; #10;
+        in = 1; #10;
+        in = 1; #10;
+        in = 0; #10;
+        in = 1; #10;
+        in = 1; #10;
+        #10 $finish;
+    end
+endmodule
