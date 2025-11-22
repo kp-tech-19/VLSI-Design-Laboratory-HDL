0047e3563019cde5ca1b04738d5c0afbef1b697b
Priyadharshini K <priyadharshinik1901@gmail.com>
2025-11-22 19:26:25 +0530

    Update Testbench.v

--- 4-Bit-Booth-Multiplier/Testbench.v
+++ 4-Bit-Booth-Multiplier/Testbench.v
@@ -9,7 +9,7 @@ module BOOTH_TESTBENCH;
 
         a = 0; b = 0; 
         #10 a = 2; b = 4; 
-        #10 a = 5; b = 8; 
+        #10 a = 5; b =-8; 
         #10 a = -3; b = 6; 
         #10 $finish; 
     end 
